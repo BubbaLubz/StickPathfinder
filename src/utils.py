42efbef1fc10f99e80122c014e3889ccf58c109c
BubbaLubz <chris.fog.city@gmail.com>
2026-02-22 02:15:54 -0800

    trail fade

--- src/utils.py
+++ src/utils.py
@@ -7,4 +7,7 @@ def draw_marker(frame, cen: tuple,):
 
 def draw_trail(frame, trail: tuple):
     for i in range(1, len(trail)):
+        alpha = 1 - (i/len(trail))
+        overlay = frame.copy()
         cv2.line(frame, trail[i-1], trail[i], (0, 0, 255), 2)
+        cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
